edb81c788df38b071ab3f17af17fbddd76840216
tunght91 <hoangtung.utc@gmail.com>
2022-03-16 23:42:37 +0700

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,1 +1,14 @@
-export { Home } from "./Home";
+import Link from "next/dist/client/link";
+import React, { FC } from "react";
+
+import { Promo } from "./ui/promo";
+
+type HomeType = {};
+
+export const Home: FC<HomeType> = () => {
+	return (
+		<>
+			<Promo />
+		</>
+	);
+};
