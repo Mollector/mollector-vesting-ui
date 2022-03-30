cb748fc1296ab72513f5eb75154ad141a87bcc67
tunght91 <hoangtung.utc@gmail.com>
2022-03-30 16:29:23 +0700

    update

--- static/js/5.65c292b0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmollector-vesting-ui"]=this["webpackJsonpmollector-vesting-ui"]||[]).push([[5],{618:function(i,o){}}]);
-//# sourceMappingURL=5.65c292b0.chunk.js.map
---
+++ static/js/5.e3cb4734.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmollector-vesting-ui"]=this["webpackJsonpmollector-vesting-ui"]||[]).push([[5],{697:function(i,o){}}]);
+//# sourceMappingURL=5.e3cb4734.chunk.js.map
