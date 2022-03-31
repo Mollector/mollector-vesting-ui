024eff4769c38ad3ca356180ca0b156285115d37
Trung Dao <trungdao912@gmail.com>
2022-03-31 16:07:12 +0700

    update UI

--- src/pages/staking/components/StakeBox/index.tsx
+++ src/pages/staking/components/StakeBox/index.tsx
@@ -4,7 +4,7 @@ import styles from '../styles.module.scss'
 
 const StakeBox: FC = () => {
   const [value, setValue] = useState('')
-  
+
   const onHandleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const stakeValue = e.target.value
     if (/^[0-9\b]+$/.test(stakeValue) || stakeValue === '') {
@@ -26,9 +26,26 @@ const StakeBox: FC = () => {
       </div>
       <div className={styles.amountText}>~ 15000 MOL + 500 BUSD</div>
       <Input value={value} onChange={onHandleChange} />
-      <button className={styles.stakeButton}>
-          Stake
-        </button>
+      <button className={styles.stakeButton}>Stake</button>
+      <div className={styles.withdrawButton}>
+        Withdraw All
+      </div>
+      <div className={styles.infoWrapper}>
+        <div className={styles.infoHeader}>
+          Info
+        </div>
+        <ul>
+          <li>
+            Min: 50 LP in 1 week
+          </li>
+          <li>
+            Reward: 1 pack
+          </li>
+        </ul>
+      </div>
+      <div className={styles.boxFooter}>
+        Don't have enough MOL. <a href="/" rel="noopener" target="_blank">Buy more</a>
+      </div>
     </div>
   )
 }
